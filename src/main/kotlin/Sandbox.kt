7f0413645d0d5d30c5763ad6c2c53b557a508b40
dgtyPedro <pedrogcs0104@gmail.com>
2022-12-09 18:00:59 -0300

    basico

--- src/main/kotlin/Sandbox.kt
+++ src/main/kotlin/Sandbox.kt
@@ -17,9 +17,54 @@ fun main() {
     // Tipagem
 
     val strQualquer: String = "Lorem"
-    val intQualquer: Int = 1000
+    val intQualquer: Int = 4
     var doubleQualquer: Double = 2.00
     // erro => val doubleErrado: Double = "yes"
     doubleQualquer += 3
     println(doubleQualquer)
+
+    testeCondicoes(intQualquer)
+    iteracoes()
+}
+
+fun iteracoes(){
+    for(i in 1..8){
+        println(i)
+    }
+    println("-----")
+    for(i in 5 downTo 1){
+        println(i)
+    }
+    println("-----")
+    for(i in 10 downTo 0 step 2){
+        if(i == 4) continue
+        if(i == 0) break
+        println(i)
+    }
+
+    println("-----")
+
+    var x = 0
+    while(x < 4){
+        println(x)
+        x++
+    }
+}
+
+fun testeCondicoes(valor: Int){
+    if(valor > 4){
+        println("$valor é maior que 4")
+    }else if(valor > 3){
+        println("$valor é maior que 3")
+    }else{
+        println("$valor é menor que 3")
+    }
+
+    when{
+        valor % 2 == 0 -> {
+            println("$valor é par")
+        }else -> {
+            println("$valor é impar")
+        }
+    }
 }
