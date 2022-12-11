63c771480fd03568248bf6b416a91312f6ef446d
dgtyPedro <pedrogcs0104@gmail.com>
2022-12-11 14:50:13 -0300

    aprendendo encapsulamento

--- src/main/kotlin/Sandbox.kt
+++ src/main/kotlin/Sandbox.kt
@@ -25,46 +25,101 @@ fun main() {
 
     testeCondicoes(intQualquer)
     iteracoes()
+
+    val minhaClasse = Classe()
+    minhaClasse.nome = "pedro"
+    minhaClasse.saldo = 100
+
+    val outraClasse = Classe()
+    outraClasse.nome = "maria"
+    outraClasse.saldo = 50
+
+    // erro -> println(outraClasse.secret)
+
+    println("------")
+    println("${minhaClasse.nome}")
+    println(outraClasse.nome)
+    outraClasse.mostrarNome()
+    if (minhaClasse.transferirSaldo(30, outraClasse)) {
+        println("Transferência finalizada")
+    } else {
+        println("Não foi possível realizar a transferência")
+    }
+
+    println(minhaClasse.getSecret())
+    outraClasse.setSecret("segredo")
 }
 
-fun iteracoes(){
-    for(i in 1..8){
+fun iteracoes() {
+    for (i in 1..8) {
         println(i)
     }
     println("-----")
-    for(i in 5 downTo 1){
+    for (i in 5 downTo 1) {
         println(i)
     }
     println("-----")
-    for(i in 10 downTo 0 step 2){
-        if(i == 4) continue
-        if(i == 0) break
+    for (i in 10 downTo 0 step 2) {
+        if (i == 4) continue
+        if (i == 0) break
         println(i)
     }
 
     println("-----")
 
     var x = 0
-    while(x < 4){
+    while (x < 4) {
         println(x)
         x++
     }
 }
 
-fun testeCondicoes(valor: Int){
-    if(valor > 4){
+fun testeCondicoes(valor: Int) {
+    if (valor > 4) {
         println("$valor é maior que 4")
-    }else if(valor > 3){
+    } else if (valor > 3) {
         println("$valor é maior que 3")
-    }else{
+    } else {
         println("$valor é menor que 3")
     }
 
-    when{
+    when {
         valor % 2 == 0 -> {
             println("$valor é par")
-        }else -> {
+        }
+
+        else -> {
             println("$valor é impar")
         }
+    }
+}
+
+class Classe {
+    var nome = ""
+    var saldo = 0;
+    private var secret = "senha perigosa"
+    //private não pode ser lida fora da classe
+
+    fun mostrarNome() {
+        println("${nome}")
+    }
+
+    fun transferirSaldo(valor: Int, destino: Classe): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.saldo += valor
+            println(saldo)
+            println(destino.saldo)
+            return true
+        }
+        return false
+    }
+
+    fun getSecret(): String{
+        return secret
+    }
+    fun setSecret(valor: String): Boolean{
+        secret = valor
+        return true
     }
 }
